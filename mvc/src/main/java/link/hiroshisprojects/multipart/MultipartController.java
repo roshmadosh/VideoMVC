7565c3be7e24734303da419e0b4366df875add63
roshmadosh <hiroshi.nobuoka@gmail.com>
2023-01-08 20:36:42 -0500

    File uploader using MultipartFile implemented for single file'

--- mvc/src/main/java/link/hiroshisprojects/multipart/MultipartController.java
+++ mvc/src/main/java/link/hiroshisprojects/multipart/MultipartController.java
@@ -1,15 +1,50 @@
 package link.hiroshisprojects.multipart;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
-@RequestMapping(value="/multipart")
 public class MultipartController {
 	
 	@GetMapping
 	public String index() {
 		return "upload";
 	}
+
+	@PostMapping("/upload")
+	public String singleFileUpload(@RequestParam("file") MultipartFile file,
+									RedirectAttributes redirectAttributes) {
+
+		if (file.isEmpty()) {
+			redirectAttributes.addFlashAttribute("message", "Upload failed. Please choose another to upload!");
+			return "redirect:uploadStatus";
+		}
+
+		try {
+			byte[] bytes = file.getBytes();
+			Path path = Paths.get("/tmp/" + file.getOriginalFilename());
+			Files.write(path, bytes);
+
+			redirectAttributes.addFlashAttribute("message", "Successfully uploaded '" + file.getOriginalFilename() + "'");
+		} catch (IOException e ) {
+			e.printStackTrace();
+		}
+
+		return "redirect:uploadStatus";
+	}
+
+	@GetMapping("/uploadStatus")
+	public String uploadStatus() {
+		return "uploadStatus";
+	}
+
 }
